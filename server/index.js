e34be96a926a2c7be8475e934f3786c9bfae1988
r4dent <113803100+r4dent@users.noreply.github.com>
2026-04-15 10:00:27 +0200

    localhost fix

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static("../public"));
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
