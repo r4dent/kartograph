9584dab893f6c8cf97eec45f783575d013a145fc
r4dent <113803100+r4dent@users.noreply.github.com>
2026-04-15 10:25:44 +0200

    path fix

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static("../public"));
+const path = require("path");
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
